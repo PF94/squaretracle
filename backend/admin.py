802123aa7ef1ca8699ac8bcf42b0088692a2e73d
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2023-01-19 11:36:09 -0500

    oh right

--- backend/admin.py
+++ backend/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import GroupAdmin
-from django.contrib.auth. admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.admin.models import LogEntry
 from django.forms import ModelForm
 from django.utils.html import format_html
